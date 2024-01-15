2ea84d73c7e18aceb14579b3c005a198d500ea8c
LUIZ MARCELO BARBOSA <luizmarcelo258@gmail.com>
2024-01-15 16:00:05 -0300

    config update

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,10 @@ async function bootstrap() {
   .setTitle('Api -To Do List')
   .setVersion('1.0')
   .build();
+
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api', app, document);
-if (module.hot) {
-  module.hot.accept();
-  module.hot.dispose(() => app.close());
-}
+
   await app.listen(5000);
 }
 bootstrap();
